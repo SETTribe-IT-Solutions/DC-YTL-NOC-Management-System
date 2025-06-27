10772adc694d27691ea03406c6585f5de80048a9
saurabhrapanwar <saurabhrapanwar@gmail.com>
2025-06-27 11:19:33 +0530

    Conn page changes

--- include/conn.php
+++ include/conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("217.21.88.4","u952673419_NOC_Portal","ST@NOCYavatmal1","u952673419_NOC_Portal");
+$conn = mysqli_connect("srv790.hstgr.io","u952673419_NOC_Portal","ST@NOCYavatmal1","u952673419_NOC_Portal");
 
 $connect = $conn;
 $con = $conn;
