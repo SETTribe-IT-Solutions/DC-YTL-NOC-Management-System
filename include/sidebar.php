64c4739e5bfbd53dc15f7b83f0574cee99c70e0e
khaled-md <161598432+khaled-md@users.noreply.github.com>
2025-07-03 19:02:59 +0530

    Changes_Files_And_Linking_NOC_Pages

--- include/sidebar.php
+++ include/sidebar.php
@@ -176,24 +176,56 @@
                         </div>
                         <!--end:Menu item-->
                         <!--begin:Menu item-->
-                        <div class="menu-item" onclick="window.location.href='#'">
+                       
+                       
+                        <!--end:Menu item-->
+                        <!--begin:Menu item-->
+                        <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
                             <!--begin:Menu link-->
-
                             <span class="menu-link">
                                 <span class="menu-icon">
-                                    <i class="ki-duotone ki-element-11 fs-1">
+                                    <i class="ki-duotone ki-rescue fs-1">
                                         <span class="path1"></span>
                                         <span class="path2"></span>
-                                        <span class="path3"></span>
-                                        <span class="path4"></span>
                                     </i>
                                 </span>
-                                <span class="menu-title">Apply NOC Application</span>
+                                <span class="menu-title">NOC Application</span>
+                                <span class="menu-arrow"></span>
                             </span>
-                            </a>
                             <!--end:Menu link-->
+                            <!--begin:Menu sub-->
+                            <div class="menu-sub menu-sub-accordion">
+                                <!--begin:Menu item-->
+                                <div class="menu-item">
+                                    <!--begin:Menu link-->
+                                    <a class="menu-link" href="officers/nocApplicationDept.php" data-bs-toggle="tooltip"
+                                        data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
+                                        <span class="menu-bullet">
+                                            <span class="bullet bullet-dot"></span>
+                                        </span>
+                                        <span class="menu-title">Department</span>
+                                    </a>
+                                    <!--end:Menu link-->
+                                </div>
+                                <!--end:Menu item-->
+                                <!--begin:Menu item-->
+                                <div class="menu-item">
+                                    <!--begin:Menu link-->
+                                    <a class="menu-link" href="civilian/nocApplication.php" data-bs-toggle="tooltip" data-bs-trigger="hover"
+                                        data-bs-dismiss="click" data-bs-placement="right">
+                                        <span class="menu-bullet">
+                                            <span class="bullet bullet-dot"></span>
+                                        </span>
+                                        <span class="menu-title">Civilian</span>
+                                    </a>
+                                    <!--end:Menu link-->
+                                </div>
+                                <!--end:Menu item-->
+
+                                <!--end:Menu item-->
+                            </div>
+                            <!--end:Menu sub-->
                         </div>
-                        <!--end:Menu item-->
                         <!--begin:Menu item-->
                         <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
                             <!--begin:Menu link-->
